f78120d33d147986641fdb470681bf9d34237d93
Laryssa Silva <laryssasiilvafs@gmail.com>
2026-01-29 20:46:40 -0300

    fix: ajustar classe active dos botoes

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
+const botoes = document.querySelectorAll(".app__card-button");
 
 function alteraContexto(contexto) {
+  botoes.forEach(function (contexto) {
+    contexto.classList.remove("active");
+  });
   html.setAttribute("data-contexto", contexto);
   banner.setAttribute("src", `/imagens/${contexto}.png`);
 
@@ -24,7 +28,7 @@ function alteraContexto(contexto) {
       titulo.innerHTML = `Hora de voltar à superfície.<br /> 
     <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
       break;
- 
+
     default:
       break;
   }
@@ -32,12 +36,15 @@ function alteraContexto(contexto) {
 
 focoBt.addEventListener("click", () => {
   alteraContexto("foco");
+  focoBt.classList.add("active");
 });
 
 curtoBt.addEventListener("click", () => {
   alteraContexto("descanso-curto");
+  curtoBt.classList.add("active");
 });
 
 longoBt.addEventListener("click", () => {
   alteraContexto("descanso-longo");
+  longoBt.classList.add("active");
 });
