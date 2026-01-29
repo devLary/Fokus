f240f9d9e8c7e4685a4f8d6b771cd1ec1e3383f3
Laryssa Silva <laryssasiilvafs@gmail.com>
2026-01-29 20:37:08 -0300

    Trocar titulo conforme modulo selecionado

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-const html = document.querySelector('html');
-const focoBt = document.querySelector('.app__card-button--foco');
-const curtoBt = document.querySelector('.app__card-button--curto');
-const longoBt = document.querySelector('.app__card-button--longo');
-const banner = document.querySelector('.app__image')
+const html = document.querySelector("html");
+const focoBt = document.querySelector(".app__card-button--foco");
+const curtoBt = document.querySelector(".app__card-button--curto");
+const longoBt = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
+const titulo = document.querySelector(".app__title");
 
 function alteraContexto(contexto) {
-    html.setAttribute('data-contexto', contexto);
-    banner.setAttribute('src', `/imagens/${contexto}.png`)
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `/imagens/${contexto}.png`);
+
+  switch (contexto) {
+    case "foco":
+      titulo.innerHTML = `Otimize sua produtividade,<br /> 
+    <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+      break;
+
+    case "descanso-curto":
+      titulo.innerHTML = `Que tal dar uma respirada?<br /> 
+    <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+      break;
+
+    case "descanso-longo":
+      titulo.innerHTML = `Hora de voltar à superfície.<br /> 
+    <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+      break;
+ 
+    default:
+      break;
+  }
 }
 
-focoBt.addEventListener('click', () => {
-  alteraContexto('foco')
+focoBt.addEventListener("click", () => {
+  alteraContexto("foco");
 });
 
-curtoBt.addEventListener('click', () => {
-  alteraContexto('descanso-curto')
+curtoBt.addEventListener("click", () => {
+  alteraContexto("descanso-curto");
 });
 
-longoBt.addEventListener('click', () => {
-  alteraContexto('descanso-longo')
+longoBt.addEventListener("click", () => {
+  alteraContexto("descanso-longo");
 });
