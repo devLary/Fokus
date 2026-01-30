5d2f4645bff67b5cc2fc4e8264deffce87d55800
Laryssa Silva <laryssasiilvafs@gmail.com>
2026-01-30 16:03:53 -0300

    Finaliza botao comecar

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const botoes = document.querySelectorAll(".app__card-button");
 const startPauseBt = document.querySelector("#start-pause");
 const musicaFocoInput = document.querySelector("#alternar-musica");
 const iniciarOuPausarBt = document.querySelector("#start-pause span");
+const iconBt = document.querySelector(".app__card-primary-butto-icon");
 
 const musica = new Audio("/sons/luna-rise-part-one.mp3");
 const audioPlay = new Audio("/sons/play.wav");
@@ -92,9 +93,13 @@ function iniciarOuPausar() {
   }
   audioPlay.play();
   intervaloId = setInterval(contagemRegressiva, 1000);
+  iniciarOuPausarBt.textContent = "Pausar";
+  iconBt.setAttribute("src", "/imagens/pause.png");
 }
 
 function zerar() {
   clearInterval(intervaloId);
+  iniciarOuPausarBt.textContent = "Começar";
+  iconBt.setAttribute("src", "/imagens/play_arrow.png");
   intervaloId = null;
 }
